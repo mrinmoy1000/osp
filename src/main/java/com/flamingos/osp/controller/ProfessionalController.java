748cca755b0dda850acfb058d29509f0f0f4d2ac
Arijit <arijitdas23@gmail.com>
2016-01-30 23:47:09 +0530

    Updated

--- src/main/java/com/flamingos/osp/controller/ProfessionalController.java
+++ src/main/java/com/flamingos/osp/controller/ProfessionalController.java
@@ -9,42 +9,38 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flamingos.osp.bean.OspProfessionalBean;
-import com.flamingos.osp.bean.UserBean;
-import com.flamingos.osp.dto.UserDTO;
-import com.flamingos.osp.exception.OspServiceException;
 import com.flamingos.osp.service.ProfessionalService;
 
 @RestController
-@RequestMapping(value="/professional")
+@RequestMapping(value = "/professional")
 public class ProfessionalController {
 
-    private static final Logger logger = Logger.getLogger(ProfessionalController.class);
+  private static final Logger logger = Logger.getLogger(ProfessionalController.class);
 
-    @Autowired
-    ProfessionalService profService;
-    
-    @RequestMapping(value = "/addProfile",produces = "application/json", method = RequestMethod.POST, consumes = "application/json")
-	public ResponseEntity<String> addProfile(@RequestBody OspProfessionalBean professionalBean,
-			HttpServletRequest request) throws Exception {
-		String successMessage = profService.saveProfile(professionalBean, request);		
+  @Autowired
+  ProfessionalService profService;
 
-		return new ResponseEntity<String>(successMessage, HttpStatus.CREATED);
-	}
-    
-    @RequestMapping(value = "/saveProfile",produces = "application/json", method = RequestMethod.POST, consumes = "application/json")
-    public ResponseEntity<String> saveProfile(@RequestBody OspProfessionalBean professionalBean,
-            HttpServletRequest request) throws Exception {
-        String successMessage = profService.saveProfile(professionalBean, request);     
+  @RequestMapping(value = "/addProfile", produces = "application/json",
+      method = RequestMethod.POST, consumes = "application/json")
+  public ResponseEntity<String> addProfile(@RequestBody OspProfessionalBean professionalBean,
+      HttpServletRequest request) throws Exception {
+    String successMessage = profService.saveProfile(professionalBean, request);
 
-        return new ResponseEntity<String>(successMessage, HttpStatus.CREATED);
-    }
-    
-   
-    
-    
+    return new ResponseEntity<String>(successMessage, HttpStatus.CREATED);
+  }
+
+  @RequestMapping(value = "/saveProfile", produces = "application/json",
+      method = RequestMethod.POST, consumes = "application/json")
+  public ResponseEntity<String> saveProfile(@RequestBody OspProfessionalBean professionalBean,
+      HttpServletRequest request) throws Exception {
+    String successMessage = profService.saveProfile(professionalBean, request);
+
+    return new ResponseEntity<String>(successMessage, HttpStatus.CREATED);
+  }
+
+
 
 }
