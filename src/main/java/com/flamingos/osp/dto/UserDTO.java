1e3aaa26d2400aa6925d865180c0defe5022d7fd
rajachakraborti <rajachakraborti@gmail.com>
2016-01-31 12:40:05 +0530

    added annotation

--- src/main/java/com/flamingos/osp/dto/UserDTO.java
+++ src/main/java/com/flamingos/osp/dto/UserDTO.java
@@ -2,6 +2,9 @@ package com.flamingos.osp.dto;
 
 import java.sql.Timestamp;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+@JsonInclude(Include.NON_NULL)
 public class UserDTO {
 
   // User_Name
